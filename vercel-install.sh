90465ed787d6949f9eecfe30efbe4e6b3c9302ac
chaonengs <99374138+chaonengs@users.noreply.github.com>
2023-05-06 03:37:58 +0000

    debug

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,5 +1,6 @@
 npm install
 yum install wget
+yum install libuuid-devel libmount-devel zlib
 wget https://github.com/madler/zlib/archive/v1.2.11.tar.gz
 tar -zxvf v1.2.11.tar.gz
 cd zlib-1.2.11
@@ -7,5 +8,5 @@ cd zlib-1.2.11
 make
 make install
 export LD_LIBRARY_PATH="/bin/zlib/lib":$LD_LIBRARY_PATH
-yum install libuuid-devel libmount-devel zlib
+ls node_modules
 cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
