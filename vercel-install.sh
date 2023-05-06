1cdb2b81f20778003ede45c4eaa97f31c10a5707
chaonengs <99374138+chaonengs@users.noreply.github.com>
2023-05-06 07:24:59 +0000

    donwgrad canvas

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,15 +1,16 @@
-npm install
-yum install wget
-yum install libuuid-devel libmount-devel zlib
-wget https://github.com/madler/zlib/archive/v1.2.9.tar.gz
-tar -zxvf v1.2.9.tar.gz
-cd zlib-1.2.9
-./configure --prefix=/bin/zlib/
-make
-make install
-export LD_LIBRARY_PATH="/bin/zlib/lib":$LD_LIBRARY_PATH
-cd ../
-cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/ 
-cp /bin/zlib//lib/libz.so.1.2.9 node_modules/canvas/build/Release/libz.so.1
-ln -s -f /usr/local/lib/libz.so.1.2.9/lib /lib64/libz.so.1
-rm node_modules/canvas/build/Release/librsvg-2.so.2
+# npm install
+# yum install wget
+# yum install libuuid-devel libmount-devel zlib
+# wget https://github.com/madler/zlib/archive/v1.2.9.tar.gz
+# tar -zxvf v1.2.9.tar.gz
+# cd zlib-1.2.9
+# ./configure --prefix=/bin/zlib/
+# make
+# make install
+# export LD_LIBRARY_PATH="/bin/zlib/lib":$LD_LIBRARY_PATH
+# cd ../
+# cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/ 
+# cp /bin/zlib//lib/libz.so.1.2.9 node_modules/canvas/build/Release/libz.so.1
+# ln -s -f /usr/local/lib/libz.so.1.2.9/lib /lib64/libz.so.1
+# rm node_modules/canvas/build/Release/librsvg-2.so.2
+npm install  --omit=optional
